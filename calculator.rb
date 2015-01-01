0123f86874614c3a63bd14159fa244d4de75b29b
Rick Peyton <peytorb@gmail.com>
2015-01-01 10:53:45 -0600

    Fixed file according to rubocop

--- calculator.rb
+++ calculator.rb
@@ -6,39 +6,40 @@ def say(message)
   puts "==> #{message}"
 end
 
-begin
-  say("Enter the first number.")
+loop do
+  say('Enter the first number.')
   number1 = gets.chomp
 
-  say("Enter the second number.")
+  say('Enter the second number.')
   number2 = gets.chomp
 
-  say("What would you like to do with these numbers?")
-  say("Enter 1 for Addition")
-  say("Enter 2 for Subtraction")
-  say("Enter 3 for Multiplication")
-  say("Enter 4 for Division")
+  say('What would you like to do with these numbers?')
+  say('Enter 1 for Addition')
+  say('Enter 2 for Subtraction')
+  say('Enter 3 for Multiplication')
+  say('Enter 4 for Division')
   response = gets.chomp
 
   if response == '1'
     answer = number1.to_i + number2.to_i
-    action = "+"
+    action = '+'
   elsif response == '2'
     answer = number1.to_i - number2.to_i
-    action = "-"
+    action = '-'
   elsif response == '3'
     answer = number1.to_i * number2.to_i
-    action = "*"
+    action = '*'
   elsif response == '4'
     answer = number1.to_f / number2.to_f
-    action = "/"
+    action = '/'
   else
-    puts "That is an invalid response."
+    puts 'That is an invalid response.'
     break
   end
 
   say("#{number1} #{action} #{number2} = #{answer}")
 
-  say("Do you want to go again?")
+  say('Do you want to go again?')
   continue = gets.chomp.upcase
-end while continue == 'Y'
+  break unless continue == 'Y'
+end
